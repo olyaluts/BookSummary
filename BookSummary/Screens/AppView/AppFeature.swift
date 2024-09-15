f471b33fb28b40fc087000d04693d65ef8bf14cc
Olga Lutsyk <olyalutsyk@gmail.com>
2024-09-15 21:11:04 +0300

    Use CasePath

--- BookSummary/Screens/AppView/AppFeature.swift
+++ BookSummary/Screens/AppView/AppFeature.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 import ComposableArchitecture
-
+import CasePaths
 
 struct AppFeature: Reducer {
     struct State {
@@ -16,28 +16,29 @@ struct AppFeature: Reducer {
         var isLoading: Bool = false
         var player: BookPlayerFeature.State?
     }
-
+   
+    @CasePathable
     enum Action {
         case player(BookPlayerFeature.Action)
-
+        
         case onAppear
         case onDisappear
         case retryButtonTapped
-
+        
         case finishLoading
         case loadError(Error)
     }
-
+    
     var body: some ReducerOf<Self> {
         Reduce { state, action in
             switch action {
             case .player: return .none
-
+                
             case .finishLoading:
                 state.isLoading = false
                 state.player = BookPlayerFeature.State(book: state.book)
                 return .none
-
+                
             case .onAppear, .retryButtonTapped:
                 state.errorMessage = nil
                 state.isLoading = true
@@ -50,17 +51,17 @@ struct AppFeature: Reducer {
                         await send(.loadError(error))
                     }
                 }
-
+                
             case .onDisappear:
                 return .none
-
+                
             case .loadError:
                 state.isLoading = false
                 state.errorMessage = "Error loading data. Please retry."
                 return .none
             }
         }
-        .ifLet(\.player, action: /Action.player) {
+        .ifLet(\.player, action: \Action.Cases.player) {
             BookPlayerFeature()
         }
     }
--- BookSummary/Screens/AppView/AppView.swift
+++ BookSummary/Screens/AppView/AppView.swift
@@ -13,25 +13,25 @@ import SwiftUI
 struct AppView: View {
     let store: StoreOf<AppFeature>
     @ObservedObject var viewStore: ViewStore<ViewState, AppFeature.Action>
-
+    
     struct ViewState: Equatable {
         let isLoading: Bool
         let errorMessage: String?
-
+        
         init(state: AppFeature.State) {
             self.isLoading = state.isLoading
             self.errorMessage = state.errorMessage
         }
     }
-
+    
     public init(store: StoreOf<AppFeature>) {
         self.store = store
         self.viewStore = ViewStore(self.store, observe: ViewState.init)
-
+        
         let thumbImage = UIImage(systemName: "circle.fill")
         UISlider.appearance().setThumbImage(thumbImage, for: .normal)
     }
-
+    
     var body: some View {
         ZStack(alignment: .bottom) {
             if let errorMessage = viewStore.errorMessage {
@@ -44,7 +44,9 @@ struct AppView: View {
                     ProgressView()
                 } else {
                     IfLetStore(
-                        self.store.scope(state: \.player, action: AppFeature.Action.player),
+                        store.scope(
+                            state: \.player, 
+                            action: AppFeature.Action.player),
                         then: BookPlayerView.init(store:)
                     )
                 }
@@ -62,6 +64,5 @@ struct AppView: View {
 #Preview {
     AppView(store: Store(initialState: AppFeature.State(book: Book.mockData)) {
         AppFeature()
-            ._printChanges()
     })
 }
