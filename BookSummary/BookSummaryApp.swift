bce4dcf0935655ba3aab4e7f91be102dcf655b58
Olga Lutsyk <olyalutsyk@gmail.com>
2024-09-15 15:52:46 +0300

    Initial Commit

---
+++ BookSummary/BookSummaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSummaryApp.swift
+//  BookSummary
+//
+//  Created by Olya Lutsyk on 15.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookSummaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
